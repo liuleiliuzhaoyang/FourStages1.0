91af9e04d39358fae6042c2792794c94118da0e3
houbingzhuo <1415487054@qq.com>
2019-05-16 20:25:11 +0800

    111

---
+++ FourStagesPojo/src/main/java/com/qf/dto/BakkDto.java
@@ -0,0 +1,13 @@
+package com.qf.dto;
+
+public class BakkDto {
+    private String aa;
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+}
